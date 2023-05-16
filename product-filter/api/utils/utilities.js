11a107d1007d893f2a18d5a3389f929b0dfd7007
De-J <devjtitan@gmail.com>
2023-05-16 17:51:43 +0530

    fix: separate multiple query params

--- product-filter/api/utils/utilities.js
+++ product-filter/api/utils/utilities.js
@@ -1,14 +1,15 @@
-export const destructure = (key, value, match_pattern = false) => {
+export const destructure = (key, value) => {
     const list = [], query = {}, obj = {}
     value.split(",").forEach(ele => {
         list.push(ele);
     });
-    if (match_pattern) {
-        let regex = list.join("|")
-        obj["$regex"] = regex;
-        obj["$options"] = "i";
-        query[key] = obj
+    // if (match_pattern) {
+    //     let regex = list.join("|")
+    //     obj["$regex"] = regex;
+    //     obj["$options"] = "i";
+    //     query[key] = obj
     }
+
     else {
         obj["$in"] = list;
         query[key] = obj;
