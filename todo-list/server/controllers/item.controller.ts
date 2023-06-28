233cc3059f0ed73b6539cf17e8e770e8311c6c59
De-J <devjtitan@gmail.com>
2023-06-28 17:39:10 +0530

    add changes

--- todo-list/server/controllers/item.controller.ts
+++ todo-list/server/controllers/item.controller.ts
@@ -1,16 +1,15 @@
 import { Request, Response } from "express";
-
+import collection from "../mongo";
 
 export const createListItem = async (req: Request , res: Response) => {
     try {
-
+        
     }
     catch (err) {
         console.error(err);
         res.status(500).send("cannot create list item")
     }
 }
-
 
 export const updateListItem = async (req: Request , res: Response) => {
     try {
